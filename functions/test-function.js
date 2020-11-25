eacda3e5c12144e427796bc99ffeeb04b596474c
James Drever <james@careful.digital>
2020-11-25 12:52:39 +0000

    test serverless story query

--- functions/test-function.js
+++ functions/test-function.js
@@ -1,7 +1,26 @@
+const sanityClient = require('@sanity/client')
+const projectId = "3vgpwmyb"
+const apiToken = ''
+const client = sanityClient({
+  projectId,
+  dataset: 'production',
+  token: '',
+  useCdn: false
+})
+
 exports.handler = async function(event, context) {
-    
+
+    const query = `
+    *[ _type == "story" && !(_id in path("drafts.**")) ]{
+       name,
+       description,
+       content,
+    } | order(publishedAt desc)
+  `
+  const params = {}
+
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify({message: client.fetch(query, params)})
     };
 }
